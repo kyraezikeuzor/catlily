733e761ff6c1f54ffe6f69a5b6f8aea39963b27a
Kyra Ezikeuzor <83562873+kyrao@users.noreply.github.com>
2022-06-03 11:01:25 -0500

    Add files via upload

---
+++ catapi.js
@@ -0,0 +1,60 @@
+//Create function ajax_get which takes a url and a callback function
+function ajax_get(url, callback) {
+    //Create the variable xmlhttp which equals a new XMLHttpRequest
+    var xmlhttp = new XMLHttpRequest(); //An XMLHttpRequest is
+    xmlhttp.onreadystatechange = function() { //xmlhttp.onreadystatechange is
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { //xmlhttp.readystate //xmlhttp.status is
+        console.log('responseText:' + xmlhttp.responseText); //xmlhttp.responseText is
+        try {
+          var data = JSON.parse(xmlhttp.responseText);
+        } catch (err) {
+          console.log(err.message + " in " + xmlhttp.responseText);
+          return;
+        }
+        callback(data);
+      }
+    };
+  
+    //Create function xml.http which takes "GET", a url, and the true boolean
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+
+document.getElementById("randomize-button").addEventListener("click", function(e){
+    e.preventDefault()
+    ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function(data) {
+        //Get the first Array object of the JSON response
+        var img = '<img src="' + data[0]["url"] + '">';
+        document.getElementById("cats-image").innerHTML = img;
+    });
+});
+
+/*
+ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function(data) {
+    //Get the first Array object of the JSON response
+    var img = '<img src="' + data[0]["url"] + '">';
+    document.getElementById("cats-image").innerHTML = img;
+    });
+
+
+  ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function(data) {
+    //Get the first Array object of the JSON response
+    var img = '<img src="' + data[0]["url"] + '">';
+    document.createElement("img") = img;
+    document.body.appendChild(img);
+});*/
+
+/*function fetchCats() {
+    fetch('https://api.thecatapi.com/v1/images/search?size=full')
+    .then(response => response.json())
+    .then((data) => {
+        let catsimgurl = data[0].url
+
+    let catsimgelement = document.createElement("img")
+    catsimgelement.setAttribute('src', `${catsimgurl}`)
+    
+    let catsimgdiv = document.querySelector("cats-image")
+    catsimgdiv.appendChild(catsimgelement)
+    })
+    .catch(err => console.log(err))
+};*/
